3e5039bcd643c7bcca61d08419b83dfc9a14b24e
BelvoixNicolas97 <belvoixnicolas1997@gmail.com>
2024-03-17 21:03:41 +0100

    Update du test de la class Cli

--- test/testCli.js
+++ test/testCli.js
@@ -3,6 +3,7 @@ const CLI = require("./../bin/Cli.js");
 async function main () {
     CLI.titre("Test de la class Cli");
     CLI.subTitre("Test de la fonction subTitre");
+    CLI.txt("Test de la fonction text");
     CLI.valid("Test de la function de la fonction valid");
     CLI.miss("Test de la fonction miss");
     CLI.inValid("Test de la fonction inValid");
