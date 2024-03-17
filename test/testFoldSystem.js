d50cba6fa4a9a7e8fe1a0696e1f51a20992b7759
BelvoixNicolas97 <belvoixnicolas1997@gmail.com>
2024-03-17 21:43:14 +0100

    Update module foldSystem

---
+++ test/testFoldSystem.js
@@ -0,0 +1,7 @@
+/**
+ * Le module permet de gérer le system de dossier de test.
+ * @module foldSystem
+ * @author Nicolas Belvoix <belvoixnicolas1997@gmail.com>
+ * @copyright Nicolas Belvoix 2022
+ * @version 1.0.0
+ */
