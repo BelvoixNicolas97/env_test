14569d3a2ffb5b808ff0105d17f1df65bc5b6174
BelvoixNicolas97 <belvoixnicolas1997@gmail.com>
2024-03-17 21:02:58 +0100

    Ecriture de la documentation de la class Cli

--- bin/Cli.js
+++ bin/Cli.js
@@ -1,5 +1,6 @@
 /**
- * Le module permet d'utiliser un environement en ligne de commande.
+ * Le module permet d'utiliser un environement en ligne de commande.</br>
+ * ![Exemple](img/utilisation.png)
  * @module cli
  * @author Nicolas Belvoix <belvoixnicolas1997@gmail.com>
  * @copyright Nicolas Belvoix 2022
@@ -8,12 +9,15 @@
 
     ///    CLASS    ///
 /**
- * La [Class Cli]{@link module:cli~Cli} permet d'intégarire avec un environement en ligne de commande.
+ * La [Class Cli]{@link module:cli~Cli} permet d'intégrer avec un environnement en ligne de commande.
  * @class
  */
 class Cli {
     /**
-     * La fonction permet d'afficher un titre.
+     * La fonction permet d'afficher un titre.</br>
+     * Affichage dans la terminale :</br>
+     * ![Titre](img/titre.png)
+     * @function
      * @param {string} txt Contenue du titre.
      */
     titre (txt) {
@@ -37,29 +41,110 @@ class Cli {
     }
 
     /**
-     * La fonction permet d'afficher un sous titre.
-     * @param {string} txt 
+     * La fonction permet d'afficher un sous-titre en affichant un point-virgule à la fin.</br>
+     * Affichage dans la terminale :</br>
+     * ![Sous-titre](img/subTitre.png)
+     * @function
+     * @param {string} txt Contenue du sous-titre.
      */
     subTitre (txt) {
         console.log(`\r\n  \x1B[4;37;40m${txt} :\x1B[0m`);
     }
 
+    /**
+     * La fonction permet d'afficher un texte pour indiquer une validation.</br>
+     * Affichage dans la terminale :</br>
+     * ![valide](img/valide.png)
+     * @function
+     * @param {string} txt Texte de la validation.
+     */
     valid (txt) {
         console.log(`    \x1B[32m${txt}\x1B[0m`);
     }
 
+    /**
+     * La fonction permet d'afficher un texte pour indiquer une erreur.</br>
+     * Affichage dans la terminale :</br>
+     * ![inValide](img/inValid.png)
+     * @function
+     * @param {string} txt Texte de l'erreur.
+     */
     inValid (txt) {
         console.log(`    \x1B[31m${txt}\x1B[0m`);
     }
 
+    /**
+     * La fonction permet d'afficher un texte pour indiquer une erreur mineure.</br>
+     * Affichage dans la terminale :</br>
+     * ![Miss](img/miss.png)
+     * @function
+     * @param {string} txt Texte de l'erreur mineur.
+     */
     miss (txt) {
         console.log(`    \x1B[33m${txt}\x1B[0m`);
     }
 
+    /**
+     * La fonction permet d'afficher un texte lambda.</br>
+     * Affichage dans la terminale :</br>
+     * ![Txt](img/txt.png)
+     * @function
+     * @param {string} txt 
+     */
     txt (txt) {
         console.log(`    ${txt}`);
     }
 
+    /**
+     * La fonction permet d'utiliser un input Boolean.</br>
+     * <table>
+     *  <thead>
+     *      <tr>
+     *          <th>Résultat</th><th>Clé clavier</th>
+     *      </tr>
+     *  </thead>
+     *  <tbody>
+     *      <tr>
+     *          <td rowspan=5>True</td>
+     *          <td>y</td>
+     *      </tr>
+     *      <tr>
+     *          <td>Y</td>
+     *      </tr>
+     *      <tr>
+     *          <td>o</td>
+     *      </tr>
+     *      <tr>
+     *          <td>O</td>
+     *      </tr>
+     *      <tr>
+     *          <td>Enter</td>
+     *      </tr>
+     *      <tr>
+     *          <td rowspan=3>False</td>
+     *          <td>n</td>
+     *      </tr>
+     *      <tr>
+     *          <td>N</td>
+     *      </tr>
+     *      <tr>
+     *          <td>Echape</td>
+     *      </tr>
+     *  </tbody>
+     * </table></br>
+     * Affichage dans la terminale :</br>
+     * <img src="img/yesOrNo.png" alt="Inpute Boolean" />
+     * @async
+     * @function
+     * @param {string} txt Texte de l'input Boollean.
+     * @returns {boolean}
+     * @example
+     * let result;
+     * result = await CLI.yesOrNo("Test de la fonction yesOrNo");
+     * 
+     * console.log(result);
+     * // Affiche true ou false.
+     */
     async yesOrNo (txt = "") {
         let yes = [
             Buffer.from([0x79]),
