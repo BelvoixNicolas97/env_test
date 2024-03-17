31d55a94be90b8ad23fbb65a6f95faec66a5d794
BelvoixNicolas97 <belvoixnicolas1997@gmail.com>
2024-03-17 21:53:14 +0100

    Update module foldSystem

---
+++ bin/foldSystem.js
@@ -0,0 +1,7 @@
+/**
+ * Le module permet d'intéragire avec le system de dossier de l'environnement de test.
+ * @module foldSystem
+ * @author Nicolas Belvoix <belvoixnicolas1997@gmail.com>
+ * @copyright Nicolas Belvoix 2022
+ * @version 1.0.0
+ */
