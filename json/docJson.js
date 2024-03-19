f35817e131092f77b6a88b2c5c290913a24e564a
BelvoixNicolas97 <belvoixnicolas1997@gmail.com>
2024-03-20 00:33:46 +0100

    Ajour de la documentation des json TXT et ERROR.

---
+++ json/docJson.js
@@ -0,0 +1,21 @@
+/**
+ * La variable est un fichier qui contient le texte du package.
+ * @global
+ * @name TXT
+ * @type {object}
+ * @property {object} foldSystem Contient le texte de la [Class FoldSystem]{@link module:foldSystem~FoldSystem}.
+ * @property {object} foldSystem.createDirPrincipal Contient le texte de [Class FoldSystem.createDirPrincipal()]{@link module:foldSystem~FoldSystem#createDirPrincipal}.
+ * @property {string} foldSystem.createDirPrincipal.errorTypePath Contient le texte d'erreur sur le type de paramétre.</br>
+ *                                                               Remplacer "@typePath@" par le type de la variable.
+ * @property {string} foldSystem.createDirPrincipal.errorPathFile Contient le texte d'erreur si le dossier est un fichier au lieu d'un dossier.</br>
+ *                                                                Remplacer "@path@" par l'url du dossier.
+ */
+
+/**
+ * @global
+ * @name ERROR
+ * @type {object}
+ * @property {string} paramType="PARAM_TYPE" Le code erreur est utiliser si le type de paramètre est erroenr.
+ * @property {string} typePath="TYPE_PATH" Le code erreur est utiliser si le type de fichier est erroner.</br>
+ *                             Il sera utiliser par exemple si il y a un fichier au lieu d'un dossier.
+ */
