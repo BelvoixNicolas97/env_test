24f704fde76bb35f4c0bd2c59b66478a0ecf8fc7
BelvoixNicolas97 <belvoixnicolas1997@gmail.com>
2024-03-19 01:02:03 +0100

    Update de la class FoldSystem

--- bin/foldSystem.js
+++ bin/foldSystem.js
@@ -3,5 +3,61 @@
  * @module foldSystem
  * @author Nicolas Belvoix <belvoixnicolas1997@gmail.com>
  * @copyright Nicolas Belvoix 2022
- * @version 1.0.0
+ * @version 1.2.0
  */
+
+const Path = require("path");
+const Fs = require("fs");
+
+const TXT = require('./../json/txt.json').foldSystem;
+
+/**
+ * @class
+ */
+class FoldSystem {
+    /**
+     * Chemin absolue du dossier de test.
+     * @type {string}
+     * @default "./outx"
+     */
+    #PATH = "./out";
+    
+    /**
+     * @constructor
+     * @param {string} path Chemin du dossier de test.
+     */
+    constructor (path) {
+        // Création du dossier
+            if (typeof path == "string") {
+                this.#PATH = this.#createDirUnic(path);
+            }else {
+                this.#PATH = this.#createDirUnic(this.#PATH);
+            }
+    }
+
+    /**
+     * La fonction va crée un dossier unique. Si le chemin indiquer existe déja. Il va incrémenter un nombre a la fin.
+     * @function
+     * @param {string} path Chemin du dossier a crée. 
+     * @returns {string} Chemin absolue du dossier crée.
+     */
+    #createDirUnic = function (path) {
+        let nbTentative = 0;
+        let pathFinale = Path.resolve(path);
+
+        // Détérmination du path
+            while (Fs.existsSync(pathFinale)) {
+                nbTentative++;
+
+                pathFinale = Path.resolve(`${path}${nbTentative}`);
+            }
+
+        // Création du dossier
+            Fs.mkdirSync(pathFinale);
+
+        // Envoie
+            return pathFinale
+    }
+}
+
+module.exports = FoldSystem;
--- test/testFoldSystem.js
+++ test/testFoldSystem.js
@@ -1,7 +1,21 @@
-/**
- * Le module permet de gérer le system de dossier de test.
- * @module foldSystem
- * @author Nicolas Belvoix <belvoixnicolas1997@gmail.com>
- * @copyright Nicolas Belvoix 2022
- * @version 1.0.0
- */
+const Cli = require("../bin/Cli.js");
+const FoldSystem = require("../bin/foldSystem.js");
+
+async function main () {
+    Cli.titre("Test de la class FoldSystem");
+        let foldSystem;
+
+    Cli.subTitre(`Test de la construction de la class`);
+        try {
+            new FoldSystem("./test/out/test");
+            foldSystem = new FoldSystem("./test/out/test");
+        } catch (error) {
+            Cli.inValid(`La class FoldSystem n'a pas put étre crée.`);
+            Cli.inValid(error);
+
+            process.exit();
+        }
+        Cli.valid(`La class FoldSystem a été crée.`);
+}
+
+main();
