a4d610b0b5cd247ea7d621049ecc73d1ff639d78
BelvoixNicolas97 <belvoixnicolas1997@gmail.com>
2024-03-20 00:03:37 +0100

    Ajout de la fonctionalité createDirPrincipal a la class FoldSystem

--- bin/foldSystem.js
+++ bin/foldSystem.js
@@ -3,24 +3,24 @@
  * @module foldSystem
  * @author Nicolas Belvoix <belvoixnicolas1997@gmail.com>
  * @copyright Nicolas Belvoix 2022
- * @version 1.2.0
+ * @version 1.3.0
  */
 
 const Path = require("path");
 const Fs = require("fs");
 
 const TXT = require('./../json/txt.json').foldSystem;
+const ERROR = require("./../json/error.json");
 
 /**
  * @class
  */
 class FoldSystem {
     /**
-     * Chemin absolue du dossier de test.
+     * Chemin absolue du dossier de test principale.
      * @type {string}
-     * @default "./outx"
      */
-    #PATH = "./out";
+    #PATH;
     
     /**
      * @constructor
@@ -28,11 +28,45 @@ class FoldSystem {
      */
     constructor (path) {
         // Création du dossier
-            if (typeof path == "string") {
-                this.#PATH = this.#createDirUnic(path);
-            }else {
-                this.#PATH = this.#createDirUnic(this.#PATH);
+            this.#createDirPrincipal(path);
+    }
+
+    /**
+     * La fonction permet de crée et d'initialiser le dossier principale de test.
+     * @function
+     * @param {string} path Chemin du dossier.
+     * @see [Class FoldSystem.PATH]{@link module:foldSystem~FoldSystem#PATH} est utilisé.
+     */
+    #createDirPrincipal = function (path) {
+        let pathAbsolue = "";
+
+        // Vérification
+            if (typeof path !== "string") {
+                let txt = TXT.createDirPrincipal.errorTypePath.replace("@typePath@", typeof path);
+                let error = new Error(txt);
+
+                error.name = ERROR.paramType;
+
+                throw error;
+            }else if (Fs.existsSync(path) && !Fs.statSync(path).isDirectory()) {
+                let txt = TXT.createDirPrincipal.errorPathFile.replace("@path@", path);
+                let error = new Error(txt);
+
+                error.name = ERROR.typePath;
+
+                throw error;
             }
+
+        // Transformation
+            pathAbsolue = Path.resolve(path);
+
+        // Création du dossier
+            if (!Fs.existsSync(pathAbsolue)) {
+                Fs.mkdirSync(pathAbsolue);
+            }
+
+        // Intégration
+            this.#PATH = pathAbsolue;
     }
 
     /**
--- test/testFoldSystem.js
+++ test/testFoldSystem.js
@@ -1,21 +1,50 @@
+const Fs = require("fs");
+
 const Cli = require("../bin/Cli.js");
 const FoldSystem = require("../bin/foldSystem.js");
 
+const PATH_DIR_TEST = "./test/out";
+
 async function main () {
     Cli.titre("Test de la class FoldSystem");
-        let foldSystem;
+        initClass();
+}
 
-    Cli.subTitre(`Test de la construction de la class`);
+function initClass () {
+    Cli.subTitre(`Initialisation de la class FoldSystem`);
         try {
-            new FoldSystem("./test/out/test");
-            foldSystem = new FoldSystem("./test/out/test");
+            new FoldSystem();
+            Cli.inValid("La class a été initialiser à vide");
+
+            process.exit();
         } catch (error) {
-            Cli.inValid(`La class FoldSystem n'a pas put étre crée.`);
+            Cli.valid("La class n'a pas été initialiser à vide");
+            Cli.valid(error);
+        }
+
+        try {
+            new FoldSystem("./package.json");
+
+            Cli.inValid(`La class a été initialiser sur la base d'un fichier`);
+        } catch (error) {
+            Cli.valid("La class n'a pas été initialiser sur la base d'un fichier");
+            Cli.valid(error);
+        }
+
+        try {
+            new FoldSystem(PATH_DIR_TEST);
+
+            if (Fs.existsSync(PATH_DIR_TEST) && Fs.statSync(PATH_DIR_TEST).isDirectory()) {
+                Cli.valid(`Le dossier "${PATH_DIR_TEST}" a été crée`);
+            }else {
+                Cli.miss(`Le dossier "${PATH_DIR_TEST}" n'a pas été crée`);
+            }
+        } catch (error) {
+            Cli.inValid("La class n'a pas été initialiser");
             Cli.inValid(error);
 
             process.exit();
         }
-        Cli.valid(`La class FoldSystem a été crée.`);
 }
 
 main();
