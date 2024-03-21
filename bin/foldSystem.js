f1eaecb355b32662bdf7a343b2d7c36d7d82ab05
BelvoixNicolas97 <belvoixnicolas1997@gmail.com>
2024-03-21 01:48:20 +0100

    Ajout de la fonction clean a la class FoldSystem

--- bin/foldSystem.js
+++ bin/foldSystem.js
@@ -3,11 +3,12 @@
  * @module foldSystem
  * @author Nicolas Belvoix <belvoixnicolas1997@gmail.com>
  * @copyright Nicolas Belvoix 2022
- * @version 1.4.0
+ * @version 1.5.0
  */
 
 const Path = require("path");
 const Fs = require("fs");
+const FS_PROMISE = require("fs/promises");
 
 const TXT = require('./../json/txt.json').foldSystem;
 const ERROR = require("./../json/error.json");
@@ -66,6 +67,99 @@ class FoldSystem {
 
         // Envoie
             return pathFinale
+    }
+
+    /**
+     * La fonction netoie le dossier de test.
+     * @async
+     * @function
+     * @returns {number} Le nombre de dossier supprimer.
+     * @see [Class FoldSystem.PATH]{@link module:foldSystem~FoldSystem#PATH} est utilisé.
+     * @see [Class FoldSystem.cleanDir]{@link module:foldSystem~FoldSystem#cleanDir} est utilisé.
+     */
+    async clean () {
+        let pathMain = this.#PATH;
+        let nbRm = 0;
+        let listPromise = [];
+        let listDir = [];
+        let result;
+
+        // Récupération de la liste des dossier
+            listDir = await FS_PROMISE.readdir(pathMain);
+
+        // Lancement du netoyage
+            for (let nameDir of listDir) {
+                let path = Path.join(pathMain, nameDir);
+
+                listPromise.push(this.#cleanDir(path));
+            }
+
+        // Attente de la fin du netoyage
+            result = await Promise.allSettled(listPromise);
+
+        // Intérprétation du résultat
+            for await (let resultPromise of result) {
+                if (resultPromise.status == "fulfilled" && resultPromise.value) {
+                    nbRm++;
+                }
+            }
+
+        // Suppresion du dossier
+            if ((await FS_PROMISE.readdir(pathMain)).length <= 0) {
+                await FS_PROMISE.rmdir(pathMain);
+            }
+
+        // Envoie
+            return nbRm;
+    }
+
+    /**
+     * La fonction supprime un dossier de test si il est vide.
+     * @async
+     * @function
+     * @param {string} path Chemin du dossier a netoyer.
+     * @returns {boolean} Renvoie true si le dossier est supprimer.
+     */
+    #cleanDir = async function (path) {
+        let result = false;
+        let nbSubDirFile = 0;
+
+        // Vérification
+            if (typeof path !== "string") {
+                let txt = TXT.cleanDir.errorTypePath.replace("typePath", typeof path);
+                let error = new Error(txt);
+
+                error.name = ERROR.paramType;
+
+                throw error;
+            }else if (!Fs.existsSync(path)) {
+                let txt = TXT.cleanDir.errorNotExist.replace("@path@", path);
+                let error = new Error(txt);
+
+                error.name = ERROR.dirNotExist;
+
+                throw error;
+            }else if (!(await FS_PROMISE.stat(path)).isDirectory()) {
+                let txt = TXT.cleanDir.errorDir.replace("@path@", path);
+                let error = new Error(txt);
+
+                error.name = ERROR.typePath;
+
+                throw error;
+            }
+
+        // Récupération du nombre de fichier
+            nbSubDirFile = (await FS_PROMISE.readdir(path)).length;
+
+        // Suppresion
+            if (nbSubDirFile <= 0) {
+                await FS_PROMISE.rmdir(path);
+
+                result = true;
+            }
+
+        // Envoie
+            return result;
     }
 
     /**
--- json/docJson.js
+++ json/docJson.js
@@ -12,6 +12,13 @@
  * @property {object} foldSystem.createDirTest Contient le texte de [Class FoldSystem.createDirTest()]{@link module:foldSystem~FoldSystem#createDirTest}.
  * @property {string} foldSystem.createDirTest.errorTypeNameDir Contient le texte d'erreur si le nom du dossier de test n'est pas une chaine de caractère.</br>
  *                                                              Remplacer "@typeNameTest@" par le nom du test.
+ * @property {object} foldSystem.cleanDir Contient le texte de [Class FoldSystem.cleanDir()]{@link module:foldSystem~FoldSystem#cleanDir}.
+ * @property {object} foldSystem.cleanDir.errorTypePath Contient le texte d'erreur si le nom du dossier a netoyer de test n'est pas une chaine de caractère.</br>
+ *                                                      Remplacer "@typePath@" par le type.
+ * @property {object} foldSystem.cleanDir.errorNotExist Contient le texte d'erreur si le dossier a netoyer n'existe pas.</br>
+ *                                                      Remplacer "@path@" par le chemin du dossier.
+ * @property {object} foldSystem.cleanDir.errorDir Contient le texte d'erreur si le dossier a netoyer n'est pas un dossier.</br>
+ *                                                      Remplacer "@path@" par le chemin du dossier.
  */
 
 /**
@@ -20,5 +27,6 @@
  * @type {object}
  * @property {string} paramType="PARAM_TYPE" Le code erreur est utiliser si le type de paramètre est erroenr.
  * @property {string} typePath="TYPE_PATH" Le code erreur est utiliser si le type de fichier est erroner.</br>
- *                             Il sera utiliser par exemple si il y a un fichier au lieu d'un dossier.
+ *                                         Il sera utiliser par exemple si il y a un fichier au lieu d'un dossier.
+ * @property {string} dirNotExist="DIR_NOT_EXIST" Le code erreur est utiliser si un dossier n'existe pas.
  */
