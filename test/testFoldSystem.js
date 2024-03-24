c6490de281a666640d85c8f227e21dead8469d91
BelvoixNicolas97 <belvoixnicolas1997@gmail.com>
2024-03-24 13:39:19 +0100

    Update du fichier de test de la class FoldSystem

--- test/testFoldSystem.js
+++ test/testFoldSystem.js
@@ -1,4 +1,5 @@
 const Fs = require("fs");
+const PATH = require("path");
 
 const Cli = require("../bin/Cli.js");
 const FoldSystem = require("../bin/foldSystem.js");
@@ -12,68 +13,134 @@ async function main () {
 }
 
 function initClass () {
-    Cli.subTitre(`Initialisation de la class FoldSystem`);
-        try {
-            new FoldSystem();
-            Cli.inValid("La class a été initialiser à vide");
+    Cli.subTitre(`Test d'initialisation de la class FoldSystem`);
+        Cli.txt(`Initialisation de la class sans url du dossier`);
+            try {
+                new FoldSystem();
+                Cli.cleanUpLine();
+                Cli.inValid("La class a été initialiser sans url de dossier");
 
-            process.exit();
-        } catch (error) {
-            Cli.valid("La class n'a pas été initialiser à vide");
-            Cli.valid(error);
-        }
-
-        try {
-            new FoldSystem("./package.json");
-
-            Cli.inValid(`La class a été initialiser sur la base d'un fichier`);
-        } catch (error) {
-            Cli.valid("La class n'a pas été initialiser sur la base d'un fichier");
-            Cli.valid(error);
-        }
-
-        try {
-            new FoldSystem(PATH_DIR_TEST);
-
-            if (Fs.existsSync(PATH_DIR_TEST) && Fs.statSync(PATH_DIR_TEST).isDirectory()) {
-                Cli.valid(`Le dossier "${PATH_DIR_TEST}" a été crée`);
-            }else {
-                Cli.miss(`Le dossier "${PATH_DIR_TEST}" n'a pas été crée`);
+                process.exit();
+            } catch (error) {
+                Cli.cleanUpLine();
+                Cli.valid("La class n'a pas été initialiser sans url de dossier");
+                Cli.valid(error);
+                Cli.txt("");
             }
-        } catch (error) {
-            Cli.inValid("La class n'a pas été initialiser");
-            Cli.inValid(error);
 
-            process.exit();
-        }
+        Cli.txt(`Initialisation de la class sur l'url d'un fichier`);
+            try {
+                new FoldSystem("./package.json");
+
+                Cli.cleanUpLine();
+                Cli.inValid(`La class a été initialiser sur l'url d'un fichier`);
+            } catch (error) {
+                Cli.cleanUpLine();
+                Cli.valid("La class n'a pas été initialiser sur l'url d'un fichier");
+                Cli.valid(error);
+                Cli.txt("");
+            }
+
+        Cli.txt(`Initialisation de la class sur l'url du dossier "${PATH_DIR_TEST}"`);
+            try {
+                new FoldSystem(PATH_DIR_TEST);
+
+                if (Fs.existsSync(PATH_DIR_TEST) && Fs.statSync(PATH_DIR_TEST).isDirectory()) {
+                    Cli.cleanUpLine();
+                    Cli.valid(`La class a été initialiser et e dossier "${PATH_DIR_TEST}" a été crée`);
+                }else {
+                    Cli.cleanUpLine();
+                    Cli.miss(`La class a été initialiser et e dossier "${PATH_DIR_TEST}" n'a pas été crée`);
+                }
+            } catch (error) {
+                Cli.cleanUpLine();
+                Cli.inValid(`La class n'a pas été initialisation sur l'url du dossier "${PATH_DIR_TEST}"`);
+                Cli.inValid(error);
+
+                process.exit();
+            }
 }
 
 function createSubDir () {
     let foldSystem;
+    let pathAbsolue;
 
     Cli.subTitre("Création d'un dossier de sous test");
-        foldSystem = new FoldSystem(PATH_DIR_TEST);
+        Cli.txt(`Initialisation de la class`);
+            try {
+                foldSystem = new FoldSystem(PATH_DIR_TEST);
+            } catch (error) {
+                Cli.cleanUpLine();
+                Cli.inValid(`La class n'a pas été initialiser sur le dossier "${PATH_DIR_TEST}"`);
+                Cli.inValid(error);
 
-    // Création d'un dossier erroner
-        try {
-            foldSystem.createDirTest(56);
+                process.exit();
+            }
+            Cli.cleanUpLine();
+            Cli.valid(`La class a été initialiser\r\n`);
 
-            Cli.inValid(`Le dossier a été crée`);
-        } catch (error) {
-            Cli.valid(`Le dossier n'a pas été crée`);
-            Cli.valid(error);
-        }
+        Cli.txt("Création d'un dossier erroner");
+            try {
+                foldSystem.createDirTest(56);
 
-    // Création d'un dossier
-        try {
-            foldSystem.createDirTest("test");
-            foldSystem.createDirTest("test");
+                Cli.cleanUpLine();
+                Cli.inValid(`Le dossier erroner a été crée`);
+                Cli.txt("");
+            } catch (error) {
+                Cli.cleanUpLine();
+                Cli.valid(`Le dossier erroner n'a pas été crée`);
+                Cli.valid(error);
+                Cli.txt("");
+            }
 
-            Cli.valid(`Le dossier a été crée`);
-        } catch (error) {
-            Cli.inValid("Le dossier de test n'a pas put étre crée");
-            Cli.inValid(error);
-        }
+        Cli.txt(`Création d'un sous dossier`);
+            try {
+                let path = PATH.resolve(PATH.join(PATH_DIR_TEST, "test"));
+                
+                pathAbsolue = foldSystem.createDirTest("test");
+
+                Cli.cleanUpLine();
+                if (!Fs.existsSync(pathAbsolue)) {
+                    Cli.inValid(`Le dossier "${pathAbsolue}" n'a pas été crée`);
+                    Cli.txt("");
+                }else if (!Fs.statSync(pathAbsolue).isDirectory()) {
+                    Cli.inValid(`Le dossier "${pathAbsolue}" n'est pas un dossier`);
+                    Cli.txt("");
+                }else if (path != pathAbsolue) {
+                    Cli.miss(`Le dossier "${pathAbsolue}" a été crée au lieu de "${path}"`);
+                    Cli.txt("");
+                }else {
+                    Cli.valid(`Le dossier "${pathAbsolue}" a été crée`);
+                    Cli.txt("");
+                }
+            } catch (error) {
+                Cli.cleanUpLine();
+                Cli.inValid("Le dossier de test n'a pas put étre crée");
+                Cli.inValid(error);
+                Cli.txt("");
+            }
+
+        Cli.txt(`Création d'un sous dossier existant`);
+            try {
+                let path = PATH.resolve(PATH.join(PATH_DIR_TEST, "test1"));
+                
+                pathAbsolue = foldSystem.createDirTest("test");
+
+                Cli.cleanUpLine();
+                if (!Fs.existsSync(pathAbsolue)) {
+                    Cli.inValid(`Le dossier existant "${pathAbsolue}" n'a pas été crée`);
+                }else if (!Fs.statSync(pathAbsolue).isDirectory()) {
+                    Cli.inValid(`Le dossier existant "${pathAbsolue}" n'est pas un dossier`);
+                }else if (path != pathAbsolue) {
+                    Cli.miss(`Le dossier existant "${pathAbsolue}" a été crée au lieu de "${path}"`);
+                }else {
+                    Cli.valid(`Le dossier existant "${pathAbsolue}" a été crée`);
+                }
+            } catch (error) {
+                Cli.cleanUpLine();
+                Cli.inValid("Le dossier existant de test n'a pas put étre crée");
+                Cli.inValid(error);
+            }
 }
 
 main();
