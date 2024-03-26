138cccc4f5677020b3ca6e6adc63f154ba303ccc
BelvoixNicolas97 <belvoixnicolas1997@gmail.com>
2024-03-26 01:22:21 +0100

    Mise en place des fichiers pour le module fileLog

---
+++ bin/fileLog.js
@@ -0,0 +1,27 @@
+/**
+ * Le module permet d'écrire dans un fichier log.
+ * @module fileLog
+ * @author Nicolas Belvoix <belvoixnicolas1997@gmail.com>
+ * @copyright Nicolas Belvoix 2024
+ * @version 1.0.0
+ */
+
+/**
+ * Contient [TXT.fileLog]{@link TXT}.
+ * @constant
+ * @type {object}
+ */
+const TXT = require('./../json/txt.json').fileLog;
+/**
+ * Contient [ERROR]{@link ERROR}.
+ */
+const ERROR = require("./../json/error.json");
+
+/**
+ * @class
+ */
+class FileLog {
+
+}
+
+module.exports = FileLog;
---
+++ test/testFileLog.js
@@ -0,0 +1,10 @@
+const Cli = require("../bin/Cli.js");
+const FileLog = require("../bin/fileLog.js");
+
+const PATH_DIR_TEST = "./test/out/testLog";
+
+async function main () {
+    Cli.titre("Test de la class FileLog");
+}
+
+main();
