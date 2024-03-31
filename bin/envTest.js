4702ffb20b8bef632a8b2b491d1dd3f45b3ddbfc
BelvoixNicolas97 <belvoixnicolas1997@gmail.com>
2024-03-31 01:49:19 +0100

    Mise en place de la branche EnvTest

---
+++ bin/envTest.js
@@ -0,0 +1,27 @@
+/**
+ * Le module permet de mettre en place un environnement de test.
+ * @module envTest
+ * @author Nicolas Belvoix <belvoixnicolas1997@gmail.com>
+ * @copyright Nicolas Belvoix 2024
+ * @version 1.0.0
+ */
+
+/**
+ * Contient [TXT.fileLog]{@link TXT}.
+ * @constant
+ * @type {object}
+ */
+const TXT = require('./../json/txt.json').fileLog;
+/**
+ * Contient [ERROR]{@link ERROR}.
+ */
+const ERROR = require("./../json/error.json");
+
+/**
+ * @class
+ */
+class EnvTest {
+
+}
+
+module.exports = EnvTest;
---
+++ test/testEnvTest.js
@@ -0,0 +1,8 @@
+const Cli = require("../bin/Cli.js");
+const EnvTest = require("../bin/envTest.js");
+
+async function main () {
+    Cli.titre("Test de la class EnvTest");
+}
+
+main();
